ab60e29cb9887683ca324fdc49e0c2db1f284180
Brian Lawhon <brianlawhon@live.com>
2022-12-29 13:53:01 -0600

    Added project page TODO: responsive and add projects

--- LandingPage/script.js
+++ LandingPage/script.js
@@ -1,5 +1,9 @@
+//Select our menu element 
 const menu = document.getElementById("menu");
 
+//Select menu items and convert to an array, append a mousemove listener to each item
+//Use them to update an active index data attribute on the menu element
+//Check out the CSS for the styling in have the data attribute is changing for patterna and image
 Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
   item.onmouseover = () => {
     menu.dataset.activeIndex = index;
